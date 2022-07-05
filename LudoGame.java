c695f853ef73c9afbd1218f5c8bdc47229a3b2bd
nuuranaflsyn <106853754+nuuranaflsyn@users.noreply.github.com>
2022-07-05 04:01:22 -0400

    Add files via upload

---
+++ LudoGame.java
@@ -0,0 +1,11 @@
+public class LudoGame {
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.initialize_board();
+        b.gameplay();
+        System.exit(0);
+
+    }
+
+}
